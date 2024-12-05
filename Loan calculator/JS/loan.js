9f7b9e6b891cad40fa260631b6e8b7ade9758b49
Nagendran-v <nagendran1810@gmail.com>
2024-12-05 09:30:26 +0530

    change loan.js

--- Loan calculator/JS/loan.js
+++ Loan calculator/JS/loan.js
@@ -4,6 +4,6 @@ const amount =  document.querySelector("#amount").value;
 const interest =  document.querySelector("#interest").value;
 const duration =  document.querySelector("#month").value;
 
-const output = amount * interest * duration;
+const output =( amount * (interest/100) )* duration;
 document.querySelector("#result").innerHTML = output;
 }
